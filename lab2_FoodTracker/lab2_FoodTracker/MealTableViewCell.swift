d490c1451017a02f14c2f676c5694bec1690f170
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-03-27 17:09:57 -0400

    lab2, doing basic UI session

---
+++ lab2_FoodTracker/lab2_FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  lab2_FoodTracker
+//
+//  Created by Mason Ko on 2019-03-27.
+//  Copyright © 2019 Mason Ko. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ lab2_FoodTracker/lab2_FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab2_FoodTracker
+//
+//  Created by Mason Ko on 2019-03-27.
+//  Copyright © 2019 Mason Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
