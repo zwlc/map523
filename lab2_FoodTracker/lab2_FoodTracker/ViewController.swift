60fd998e69b48d611433bfaee974e7a7ea5ceed9
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-03-27 19:43:30 -0400

    ready to start 'Display a Default Photo'

--- lab2_FoodTracker/lab2_FoodTracker/ViewController.swift
+++ lab2_FoodTracker/lab2_FoodTracker/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
     }
-
+    
+    // MARK: UITextFieldDelegate
+    
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+//        if(nameTextField.text != ""){
+//            mealNameLabel.text = nameTextField.text
+//        }
+//        else
+//        {
+//            mealNameLabel.text = "Meal Name"
+//        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
 
 }
 
