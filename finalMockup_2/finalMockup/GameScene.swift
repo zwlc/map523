93cebd7d834006659253aacd526d273d324af686
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-04-10 03:05:33 -0400

    bomb added, kinda works

--- finalMockup_2/finalMockup/GameScene.swift
+++ finalMockup_2/finalMockup/GameScene.swift
@@ -20,6 +20,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     var net: SKSpriteNode!
     var allScore: [Int] = []
     var finalScore: SKLabelNode!
+    var bomb: SKSpriteNode!
     
     var score : Int = 0
     var life = 3
@@ -61,8 +62,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         addScoreLabel()
         addLifeLabel()
         addNet()
+        addBomb()
 
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.gravity = CGVector(dx: 2.5, dy: 0)
         ballTimer = Timer.scheduledTimer(timeInterval: 0.50, target: self, selector: #selector(addBall), userInfo: nil, repeats: true)
     }
     
@@ -70,7 +72,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         racket = SKSpriteNode(imageNamed: "racket")
         racket.position = CGPoint(x: 0, y: -self.size.height / 2.5)
         racket.size = CGSize(width: 100, height: 100)
-        racket.position = CGPoint(x: 0, y: -self.size.height/2.5)
         racket.physicsBody = SKPhysicsBody(rectangleOf: racket.size)
         racket.physicsBody?.affectedByGravity = false
         racket.physicsBody?.mass = 10000
@@ -102,7 +103,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         net.physicsBody?.categoryBitMask = netCategory
         net.physicsBody?.contactTestBitMask = ballCategory
     }
-    
+    @objc func addBomb(){
+        bomb = SKSpriteNode(imageNamed: "bomb")
+        bomb.position = CGPoint(x: -300, y: -self.size.height / 2.5)
+        bomb.size = CGSize(width: 50, height: 50)
+        bomb.physicsBody = SKPhysicsBody(rectangleOf: bomb.size)
+        bomb.physicsBody?.affectedByGravity = true
+        bomb.physicsBody?.mass = 10000
+        
+        let action = SKAction.moveTo(y: 500, duration: 5)
+        bomb.run(SKAction.repeatForever(action))
+        
+        self.addChild(bomb)
+    }
     
     @objc func addBall () {
         
@@ -134,7 +147,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         
         ball.physicsBody = SKPhysicsBody(rectangleOf: ball.size)
         ball.physicsBody?.isDynamic = true
-        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.affectedByGravity = true
         ball.physicsBody?.categoryBitMask = ballCategory
         ball.physicsBody?.contactTestBitMask = racketCategory | netCategory
         ball.physicsBody?.collisionBitMask = 1
